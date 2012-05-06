6ae7cea8bba9951e01dc052e72d3122f88833cc3
Sanja Popovic <sanja89@gmail.com>
2012-05-06 17:22:53 -0400

    Added multiplication hash

--- multiplicationHash/multiplicationHash.cpp
+++ multiplicationHash/multiplicationHash.cpp
@@ -1,16 +1,27 @@
 #include "multiplicationHash.h"
 
-MultiplicationHash::MultiplicationHash()
+MultiplicationHash::MultiplicationHash(int size)
 {
-
+  srand(time(NULL));
+  this->size = size;
+  this->p = 2305843009213693951ULL;
+  this->a = (((int64)rand() << 32) | rand()) % (p-1) + 1; // guaranteed to be > 0 and < p
+  this->b = (((int64)rand() << 32) | rand()) % p;
 }
 
 int64 MultiplicationHash::hash(int64 key)
 {
-  return 0;
+  return ((a * key + b) % p) % size;
 }
 
 int64 MultiplicationHash::hash(string key)
 {
-  return 0;
+  int64 a2 = (((int64)rand() << 32) | rand()) % (p-1) + 1;
+  int64 pow = 1;
+  int64 sum = 0;
+  for (unsigned int i = 0; i < key.length(); i++) {
+    sum += key[i] * pow % p;
+    pow *= a2;
+  }
+  return this->hash(sum);
 }
